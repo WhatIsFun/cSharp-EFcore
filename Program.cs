bdf9417bc233dda6dbf21e84b17a57c88129d58d
WhatIsFun <mohammed110051@gmail.com>
2023-09-18 14:23:53 +0400

    Insert into the DB

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var _context = new ApplicationDbContext();
+            //var emp1 = new Employee { EmployeeName = "Mohammed", EmployeeAge = 24 };
+            //var emp2 = new Employee { EmployeeName = "Ahmed", EmployeeAge = 26 };
+            //var emp3 = new Employee { EmployeeName = "Said", EmployeeAge = 30 };
+
+            //_context.Add(emp1);
+            //_context.Add(emp2);
+            //_context.Add(emp3);
+
+            Employee emp = _context.Employees.Where(y => y.Id == 1).FirstOrDefault();
+            emp.EmployeeName = "Matar";
+            _context.Update(emp);
+            _context.SaveChanges();
+
         }
     }
 }
