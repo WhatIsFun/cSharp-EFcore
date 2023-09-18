c3eb0e9cb17039c73aa2a02698b0e678053dbd93
WhatIsFun <mohammed110051@gmail.com>
2023-09-18 13:47:26 +0400

    cSharp-EFcore

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EFConsole
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer("Data Source=(local);Initial Catalog=EFCore; Integrated Security=true; TrustServerCertificate=True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ Migrations/20230918081323_EFCore.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFConsole.Migrations
+{
+    /// <inheritdoc />
+    public partial class EFCore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
